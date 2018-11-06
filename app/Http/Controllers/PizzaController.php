8f0430df1d4dd50fc6b4f849180168bbcd22957c
Rob Cooper <sunscreem@gmail.com>
2018-11-06 19:04:34 +0000

    added scout

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -15,7 +15,7 @@ class PizzaController extends Controller
     public function search(Request $request)
     {
         $results = ['test1','test2','test3'];
-              
+      
         return view('pages.pizza')->with(compact('results'));
     }
 
@@ -44,6 +44,8 @@ class PizzaController extends Controller
 
 
 
+
+// $results = PizzaTopping::search($request->input('q'))->get()->pluck('topping');
 
 
 // $results = PizzaTopping::where('topping', 'like', '%' . $request->input('q') . '%')->pluck('topping');
